c58984684a1a80a4499f7f44dfc594621a58a02b
Jamie <jamiesullivan523@gmail.com>
2019-06-30 19:03:19 +0100

    Added navigation transition and fixed padding

--- src/public/js/client/navigation.js
+++ src/public/js/client/navigation.js
@@ -1,34 +1,49 @@
 const $ = require('jquery');
 const tabletSize = 900;
 
-let menuBtn;
+let navigation;
 let navGroup;
+let menuBtn;
 let footerBtn;
+let showMenu = false;
 
-window.onresize = getWindowSize;
+window.onresize = toggleNavigationClasses;
 
 $(document).ready(() => {
-  menuBtn = document.querySelector("#menu-btn");
+  navigation = document.querySelector('.navigation');
   navGroup = document.querySelector('.navigation__group');
+  menuBtn = document.querySelector("#menu-btn");
   footerBtn = document.querySelector('#footer-icon-btn');
-  menuBtn.addEventListener('click', showMenu);
+  menuBtn.addEventListener('click', toggleMenu);
   footerBtn.addEventListener('click', scrollTop);
+  toggleNavigationClasses();
 });
 
-function getWindowSize() {
+function toggleNavigationClasses() {
   if (window.innerWidth > tabletSize) {
-    navGroup.classList.remove('menu-is-active');
+    navGroup.classList.remove('show-menu');
+    navGroup.classList.remove('hide-menu');
+  }
+
+  window.innerWidth > tabletSize ? navigation.classList.add('wrapper') : navigation.classList.remove('wrapper');
+}
+
+function toggleMenu() {
+  showMenu = !showMenu;
+
+  if (showMenu) {
+    navGroup.classList.add('show-menu');
+    navGroup.classList.remove('hide-menu');
+  } else {
+    navGroup.classList.remove('show-menu');
+    navGroup.classList.add('hide-menu');
   }
 }
 
-function showMenu() {
-  navGroup.classList.toggle('menu-is-active');
-}
-
-var scrollTop = function (e) {
+function scrollTop() {
   window.scroll({
     top: 0,
     left: 0,
     behavior: 'smooth'
   });
-};
+}
